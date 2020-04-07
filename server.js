0bd388411445bf5bd35d3e8490628e49562dc3b6
FelipeEnne <felipeenne@gmail.com>
2020-04-07 15:47:25 -0300

    fix the server.js

--- server.js
+++ server.js
@@ -1,47 +1,13 @@
 const path = require('path');
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const dir = path.join('./dist');
+const app = express();
+const port = process.env.PORT || 5000;
 
-const mime = {
-  html: 'text/html',
-  txt: 'text/plain',
-  css: 'text/css',
-  gif: 'image/gif',
-  jpg: 'image/jpeg',
-  png: 'image/png',
-  svg: 'image/svg+xml',
-  js: 'application/javascript',
-};
+app.use(express.static(`${__dirname}/dist`));
 
-// eslint-disable-next-line consistent-return
-const server = http.createServer((req, res) => {
-  const reqpath = req.url.toString().split('?')[0];
-  if (req.method !== 'GET') {
-    res.statusCode = 501;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('Method not implemented');
-  }
-  const file = path.join(dir, reqpath.replace(/\/$/, '/index.html'));
-  if (file.indexOf(dir + path.sep) !== 0) {
-    res.statusCode = 403;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('Forbidden');
-  }
-  const type = mime[path.extname(file).slice(1)] || 'text/plain';
-  const s = fs.createReadStream(file);
-  s.on('open', () => {
-    res.setHeader('Content-Type', type);
-    s.pipe(res);
-  });
-  s.on('error', () => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 404;
-    res.end('Not found');
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-server.listen(8080, () => {
-  console.log('Listening on http://localhost:8080/');
-});
+app.listen(port);
