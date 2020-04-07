25e60ce6c555f1f2fb5c4f3adf8245a11943f658
FelipeEnne <felipeenne@gmail.com>
2020-04-07 15:56:12 -0300

    change server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const path = require('path');
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 
+const path = require('path');
+const serveStatic = require('serve-static');
+
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
-app.use(express.static(`${__dirname}/dist`));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
+const port = process.env.PORT || 8000;
 app.listen(port);
+
+console.log(`server started ${port}`);
