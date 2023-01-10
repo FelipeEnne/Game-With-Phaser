ecf7ed2b6260ee83a7862034f2cd10c150a99c8e
FelipeEnne <felipeenne@gmail.com>
2023-01-09 21:06:00 -0300

    update vercel add 5

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/../dist`));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
