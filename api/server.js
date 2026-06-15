121fd91c4911694e658aed0e613ec79ceee91941
FelipeEnne <felipeenne@gmail.com>
2026-06-15 19:03:24 -0300

    fix: unkonwn routes

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(`${__dirname}/../dist`));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.resolve(__dirname, "../dist/index.html"));
 });
 
 app.listen(port);
